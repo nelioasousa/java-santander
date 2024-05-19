bcf0c4308ec280446f5780b5a3a13fc1d56517d4
nelioasousa <corneliosousaphb@gmail.com>
2024-05-19 19:25:35 -0300

    Refactor & rename syntax project

--- lab-syntax/src/ContaTerminal.java
+++ lab-syntax/src/ContaTerminal.java
@@ -7,17 +7,17 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         try {
             // Doesn't make much sense! But there it is...
-            int accountNumber = ContaTerminal.askInt("Número da conta: ");
-            String bankBranch = ContaTerminal.askText("Agência: ");
-            String fullName = ContaTerminal.askText("Nome: ");
-            double accountBalance = ContaTerminal.askDouble("Saldo: ");
+            int accountNumber = askInt("Número da conta: ");
+            String bankBranch = askText("Agência: ");
+            String fullName = askText("Nome: ");
+            double accountBalance = askDouble("Saldo: ");
             System.out.printf(
                 "%nOlá %s, obrigado por criar uma conta em nosso banco,"
                 + " sua agência é %s, conta %s e seu saldo"
                 + " %.2f já está disponível para saque.%n",
                 fullName, bankBranch, accountNumber, accountBalance);
         } finally {
-            ContaTerminal.scanner.close();
+            scanner.close();
         }
     }
     
@@ -27,7 +27,7 @@ public class ContaTerminal {
             try {
                 System.out.print(message);
                 value = Integer.parseInt(
-                    ContaTerminal.scanner.nextLine().trim());
+                    scanner.nextLine().trim());
                 break;
             }
             catch (NumberFormatException e) {
@@ -43,7 +43,7 @@ public class ContaTerminal {
             try {
                 System.out.print(message);
                 value = Double.parseDouble(
-                    ContaTerminal.scanner.nextLine().trim());
+                    scanner.nextLine().trim());
                 break;
             }
             catch (NumberFormatException e) {
@@ -57,7 +57,7 @@ public class ContaTerminal {
         String text;
         do {
             System.out.print(message);
-            text = ContaTerminal.scanner.nextLine().trim();
+            text = scanner.nextLine().trim();
         } while (text.length() == 0);
         return text;
     }
