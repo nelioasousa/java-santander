763903aff5e588ff526a703a1c9af34d6139c6b3
nelioasousa <corneliosousaphb@gmail.com>
2024-06-26 13:34:56 -0300

    mongodb QuickStartPojoExample for working with POJOs

---
+++ mongodb-example/src/main/java/com/example/QuickStartPojoExample.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static com.mongodb.client.model.Filters.eq;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+/* 
+ * Code from https://www.mongodb.com/docs/drivers/java/sync/current/quick-start/
+ */
+public class QuickStartPojoExample {
+
+    public static void main(String[] args) {
+        CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+
+        String uri = "<connection-string>";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix").withCodecRegistry(pojoCodecRegistry);
+            MongoCollection<Movie> collection = database.getCollection("movies", Movie.class);
+
+            Movie movie = collection.find(eq("title", "Back to the Future")).first();
+            System.out.println(movie);
+        }
+    }
+}
